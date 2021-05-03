cea211f4812191a9474aac21488fb3293c459b93
marymary-s <belqeesboulefa@gmail.com>
2021-05-03 11:05:59 +0100

    001

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var button = document.getElementById("search-button");
 var query = document.getElementById("Ssearch-text");
-var sterm ="";
+var sterm="";
+
+button.addEventListener("click",afterbutton);
+query.addEventListener("keypress",afterkeypress);
 
 
 function afterbutton(){
-        if (query.value.length > 0) {
-            sterm = query.value;// send it to search engine 
-            query.value="";
-            } }
-
-function afterkeypress(Event){
-    if (query.value.length > 0 && Event.keycode ===13) {
+    if (query.value.length > 0) {
         sterm = query.value;// send it to search engine 
-        query.value="";
+       //query.value="";
         } }
 
-button.addEventListner("click",afterbutton);
-query.addEventListner("keypress",afterkeypress);
+function afterkeypress(event){
+if (query.value.length > 0 && event.which ===13) {
+    sterm = query.value;// send it to search engine 
+    //query.value="";
+    console.log(sterm);
+
+    } }
+
